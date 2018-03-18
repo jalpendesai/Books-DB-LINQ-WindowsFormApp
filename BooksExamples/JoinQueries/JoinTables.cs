6787564287592d2c80155435fd89d2c9fddfaca9
jalpendesai <djalpen24@gmail.com>
2018-03-17 21:00:36 -0700

    Updated Queries

--- BooksExamples/JoinQueries/JoinTables.cs
+++ BooksExamples/JoinQueries/JoinTables.cs
@@ -21,39 +21,54 @@ namespace JoinQueries
 
         private void JoinTables_Load(object sender, EventArgs e)
         {
-            //get authors and ISBNs of each book they co-authored
-            var authorAndISBNs = from author in bookDb.Authors
-                                 from book in bookDb.Titles
-                                 orderby author.LastName, author.FirstName
-                                 select new { author.FirstName, author.LastName, book.ISBN };
-            outputTextBox.AppendText("Authors and Books:");
-            foreach (var item in authorAndISBNs)
-            {
-                outputTextBox.AppendText(String.Format("\r\n\t{0,-10} {1,-10} {2,-10}", item.FirstName,item.LastName, item.ISBN ));
-            }
-
-            //get authors and titiles of each book they co-authored
+            //1
+        
             var authorsAndTitles = from book in bookDb.Titles
-                                   from author in bookDb.Authors
-                                   orderby author.LastName, author.FirstName, book.Title1
+                                   from author in book.Authors
+                                   orderby book.Title1
                                    select new { author.FirstName, author.LastName, book.Title1 };
-            outputTextBox.AppendText("\r\n\r\n Authors and titles:");
+            outputTextBox.AppendText("\r\n\r\n Sorted by Title:\n");
             foreach (var item in authorsAndTitles)
             {
                 outputTextBox.AppendText(string.Format("\r\n\t{0,-10} {1,-10} {2}", item.FirstName, item.LastName, item.Title1));
             }
 
-            //get authors and titles of each book
-            var titleByAuthor = from author in bookDb.Authors
-                                let Name = author.FirstName + " " + author.LastName
-                                orderby author.LastName, author.FirstName
-                                select new
-                                {
-                                    Name,
-                                    Titles = from book in author.Titles
-                                             orderby book.Title1
-                                             select book.Title1
-                                };
+            //2
+            
+            var authorsAndTitlesSorted = from book in bookDb.Titles
+                                   from author in book.Authors
+                                   orderby book.Title1, author.LastName, author.FirstName
+                                   select new { author.FirstName, author.LastName, book.Title1 };
+            outputTextBox.AppendText("\r\n\r\n Sorted by Title and Author:\n");
+            foreach (var item in authorsAndTitlesSorted)
+            {
+                outputTextBox.AppendText(string.Format("\r\n\t{0,-10} {1,-10} {2}", item.FirstName, item.LastName, item.Title1));
+            }
+
+            
+
+            //3
+            outputTextBox.AppendText("\r\n\r\n Grouped by Title, sorted by title and Author:\n");
+            var allTitles = from book in bookDb.Titles
+                            orderby book.Title1
+                            select book;
+            if (allTitles.Any())
+            {
+                foreach (Title title in allTitles)
+                {
+                    outputTextBox.AppendText(string.Format("\n{0}\r\n", title.Title1));
+                    var titleAuthors = from author in title.Authors
+                                       orderby author.LastName, author.FirstName
+                                       select author;
+                    if (titleAuthors.Any())
+                    {
+                        foreach(Author author in titleAuthors)
+                        {
+                            outputTextBox.AppendText(string.Format("\t\t\t{0}\r\n", author.FirstName + " " + author.LastName));
+                        }
+                    }
+                }
+            }
 
         }
     }
