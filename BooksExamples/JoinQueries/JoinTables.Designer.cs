13b572b29e84149dbb6422ac028fa00ae06b94d9
jalpendesai <djalpen24@gmail.com>
2018-03-17 19:41:48 -0700

    Added DataBase and Queries

---
+++ BooksExamples/JoinQueries/JoinTables.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace JoinQueries
+{
+    partial class JoinTables
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.outputTextBox.Font = new System.Drawing.Font("Lucida Console", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputTextBox.Location = new System.Drawing.Point(26, 44);
+            this.outputTextBox.Multiline = true;
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.outputTextBox.Size = new System.Drawing.Size(872, 529);
+            this.outputTextBox.TabIndex = 0;
+            // 
+            // JoinTables
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(910, 585);
+            this.Controls.Add(this.outputTextBox);
+            this.Name = "JoinTables";
+            this.Text = "Join Tables";
+            this.Load += new System.EventHandler(this.JoinTables_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox outputTextBox;
+    }
+}
+
---
+++ BooksExamples/JoinQueries/JoinTables.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BooksExamples;
+
+namespace JoinQueries
+{
+    public partial class JoinTables : Form
+    {
+        BooksEntities bookDb = new BooksEntities();
+        public JoinTables()
+        {
+            InitializeComponent();
+        }
+
+        private void JoinTables_Load(object sender, EventArgs e)
+        {
+            //get authors and ISBNs of each book they co-authored
+            var authorAndISBNs = from author in bookDb.Authors
+                                 from book in bookDb.Titles
+                                 orderby author.LastName, author.FirstName
+                                 select new { author.FirstName, author.LastName, book.ISBN };
+            outputTextBox.AppendText("Authors and Books:");
+            foreach (var item in authorAndISBNs)
+            {
+                outputTextBox.AppendText(String.Format("\r\n\t{0,-10} {1,-10} {2,-10}", item.FirstName,item.LastName, item.ISBN ));
+            }
+
+            //get authors and titiles of each book they co-authored
+            var authorsAndTitles = from book in bookDb.Titles
+                                   from author in bookDb.Authors
+                                   orderby author.LastName, author.FirstName, book.Title1
+                                   select new { author.FirstName, author.LastName, book.Title1 };
+            outputTextBox.AppendText("\r\n\r\n Authors and titles:");
+            foreach (var item in authorsAndTitles)
+            {
+                outputTextBox.AppendText(string.Format("\r\n\t{0,-10} {1,-10} {2}", item.FirstName, item.LastName, item.Title1));
+            }
+
+            //get authors and titles of each book
+            var titleByAuthor = from author in bookDb.Authors
+                                let Name = author.FirstName + " " + author.LastName
+                                orderby author.LastName, author.FirstName
+                                select new
+                                {
+                                    Name,
+                                    Titles = from book in author.Titles
+                                             orderby book.Title1
+                                             select book.Title1
+                                };
+
+        }
+    }
+}
